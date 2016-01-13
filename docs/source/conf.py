9eb696386daebc92c0e2d0aaa1edeaf3cfa6392e
Oskar Weser <oskar.weser@gmail.com>
2016-01-13 17:58:15 +0100

    asdfasd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,8 +21,8 @@ from unittest.mock import MagicMock
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Mock object for being able to import modules that are in e.g. C
