d189e340c1bc3294dfc119d561d39f54e31ce63e
Oskar Weser <oskar.weser@gmail.com>
2016-02-24 15:56:43 +0100

    preparing pip installation

--- setup.py
+++ setup.py
@@ -1,12 +1,54 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup file for the chemcoord package.
+"""
 
-setup(name='chemcoord',
-      version='1.0',
-      description='A package to work with chemical coordinates',
-      license='MIT',
-      author='Oskar Weser',
-      author_email='oskar.weser@gmail.com',
-      url='https://github.com/mcocdawc/chemcoord',
-      packages=['chemcoord'],
-      requires=['numpy', 'pandas', 'copy', 'math', 'collections', 'os', 'sys'],
-     )
+from setuptools import setup, find_packages
+
+
+MAIN_PACKAGE = 'chemcoord'
+DESCRIPTION = "Python module for dealing with chemical coordinates."
+VERSION='1.0.0'
+LICENSE='GPL'
+AUTHOR='Oskar Weser'
+EMAIL='oskar.weser@gmail.com'
+URL='https://github.com/mcocdawc/chemcoord'
+#PACKAGES=['chemcoord'],
+REQUIRES=['numpy', 'pandas', 'copy', 'math', 'collections', 'os', 'sys']
+KEYWORDS = ['chemcoord', 'transformation', 'cartesian', 'internal', 'chemistry', 'zmatrix', 'xyz', 'zmat']
+
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Environment :: Console',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: GPL',
+               'Natural Language :: English',
+               'Programming Language :: Python :: 3',
+               'Topic :: Scientific/Engineering :: Chemistry',
+               'Topic :: Scientific/Engineering :: Physics']
+
+
+def readme():
+    '''Return the contents of the README.rst file.'''
+    with open('README') as freadme:
+        return freadme.read()
+
+
+def setup_package():
+    setup(name=MAIN_PACKAGE,
+        version=VERSION,
+        url=URL,
+        description=DESCRIPTION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        include_package_data=True,
+        keywords=KEYWORDS,
+        license=LICENSE,
+        long_description=readme(),
+        classifiers=CLASSIFIERS,
+        packages=find_packages(),
+        requires=REQUIRES
+        )
+
+
+if __name__ == "__main__":
+    setup_package()
