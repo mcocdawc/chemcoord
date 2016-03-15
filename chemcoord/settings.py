dd73a549a6d55c68a6cadb0bb8716f499f9c296a
Oskar Weser <oskar.weser@gmail.com>
2016-03-15 21:14:52 +0100

    rtd

--- chemcoord/settings.py
+++ chemcoord/settings.py
@@ -4,4 +4,4 @@ show_warnings = {'valency' : True}
 # The Cartesian().get_bonds() method will use or not use a lookup.
 # Greatly increases performance if True, but could introduce bugs
 # if the Cartesian().xyz_frame is changed manually.
-internally_use_lookup = True
+use_lookup_internally = True
