a41c8577c60aeced8d23336eefed143620fc181c
mcocdawc <oskar.weser@gmail.com>
2017-05-15 15:23:02 +0200

    Small change to setup.py

--- setup.py
+++ setup.py
@@ -32,8 +32,9 @@ CLASSIFIERS = [
 
 
 def give_version():
-    # Fetch version from git tags, and write to version.py.
-    # Also, when git is not available (PyPi package), use stored version.py.
+    """Fetch version from git tags, and write to version.py.
+    Also, when git is not available (PyPi package), use stored version.py.
+    """
     version_py = os.path.join('.', 'version.py')
 
     try:
