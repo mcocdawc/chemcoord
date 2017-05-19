a7b25fae38f764663bee0fb318b8e1a68825fcb1
mcocdawc <oskar.weser@gmail.com>
2017-05-19 17:21:44 +0200

    started pytest

---
+++ chemcoord/tests/_generic_classes/pandas_wrapper.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from chemcoord._generic_classes._pandas_wrapper import _pandas_wrapper
+
+df_data = pd.DataFrame(np.random.rand(3, 2), columns=['A', 'B'])
+data = _pandas_wrapper(df_data)
+
+
+def test_loc():
+    assert df_data.loc[:, :] == data.loc[:, :]
