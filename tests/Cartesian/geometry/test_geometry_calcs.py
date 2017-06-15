2e0193ba3d7c23a611ef5d77a2e819a6d5668b66
mcocdawc <oskar.weser@gmail.com>
2017-06-15 16:57:21 +0200

    TEST: tests added

--- tests/Cartesian/geometry/test_geometry_calcs.py
+++ tests/Cartesian/geometry/test_geometry_calcs.py
@@ -1,7 +1,6 @@
 import chemcoord as cc
 import pytest
 import numpy as np
-from numpy import isclose
 import os
 import sys
 
@@ -27,5 +26,22 @@ molecule = cc.Cartesian.read_xyz(os.path.join(STRUCTURES, 'MIL53_small.xyz'))
 
 
 def test_bond_length():
-    assert isclose(molecule.bond_lengths([0, 1])[0], 1.3041493563710411)
-    assert isclose(molecule.bond_lengths([0, 1])[0], 1.3041493563710411)
+    calculated = molecule.bond_lengths([0, 1])[0]
+    expctd_res = 1.3041493563710411
+    assert np.allclose(calculated, expctd_res)
+
+    calculated = molecule.bond_lengths([[0, 1], [1, 2]])
+    expct_res = np.array([1.30414936, 3.25813467])
+    assert np.allclose(calculated, expct_res)
+
+
+def test_fragmentate():
+    fragments = molecule.fragmentate()
+    assert len(fragments) == 1
+    assert fragments[0] == molecule
+
+
+def test_shortest_distance():
+    i, j, d = molecule.shortest_distance(molecule + [0, 0, 10])
+    assert (i, j) == (27, 24)
+    assert np.allclose(d, 4.2537465795414988)
