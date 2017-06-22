44e8c56528342d3eaf2e911fdfac54d8349aba5a
mcocdawc <oskar.weser@gmail.com>
2017-06-22 12:15:51 +0200

    MAINT: added reference to bug in build_conda_package.sh

--- conda.recipe/build_conda_packages.sh
+++ conda.recipe/build_conda_packages.sh
@@ -13,10 +13,6 @@ do
   py_version_dir="${root_dir}/${py_version}"
   package_dir="${py_version_dir}/${system}"
 
-  # The following is a workaround because of this bug:
-  # https://github.com/conda/conda-build/issues/1957
-  # intended would be:
-  # package_path=$(conda-build --python $py_version --output-folder $package_dir --output .)
   package_name=$(conda-build --python $py_version --output .)
   package_name=${package_name##*/}
   package_path="$package_dir/$package_name"
@@ -28,3 +24,29 @@ do
     anaconda upload $py_version_dir/$platform/$package_name
   done
 done
+
+
+
+# ================================================
+# I used a workaround above because of this bug:
+# https://github.com/conda/conda-build/issues/1957
+# intended would be:
+
+
+# root_dir=$(conda-build --output .)
+# root_dir=${root_dir%/*/*}
+#
+# for py_version in '2.7' '3.5' '3.6'
+# do
+#   py_version_dir="${root_dir}/${py_version}"
+#
+#   package_path=$(conda-build --python $py_version --output-folder $py_version_dir --output .)
+#   package_name=${package_path##*/}
+#
+#   conda-build --no-anaconda-upload --python $py_version --output-folder $py_version_dir .
+#   for platform in 'osx-64' 'linux-32' 'linux-64' 'win-32' 'win-64'
+#   do
+#     conda-convert -p $platform -o $py_version_dir $package_path
+#     anaconda upload $py_version_dir/$platform/$package_name
+#   done
+# done
