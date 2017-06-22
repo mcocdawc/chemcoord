ae781f3360691cc1d4a654d5cb4f9dc0694dd7d3
mcocdawc <oskar.weser@gmail.com>
2017-06-22 12:48:46 +0200

    MAINT: Cleaned build_conda_packages.sh

--- conda.recipe/build_conda_packages.sh
+++ conda.recipe/build_conda_packages.sh
@@ -1,52 +1,27 @@
 #!/bin/sh
 
-# anaconda login
+# ====================================================
+# Please note the bug, that for conda-build the option '--output' does
+# not respect the directories given by '--output-folder':
+# https://github.com/conda/conda-build/issues/1957
+# ====================================================
 
-root_dir=$(conda-build --output .)
-root_dir=${root_dir%/*}
-system=${root_dir##*/}
-root_dir=${root_dir%/*}
+tmp=$(dirname $(conda-build --output .))
+system=$(basename $tmp)
+root_dir=$(dirname $tmp)
 
 
 for py_version in '2.7' '3.5' '3.6'
 do
-  py_version_dir="${root_dir}/${py_version}"
-  package_dir="${py_version_dir}/${system}"
+  package_name=$(basename $(conda-build --python ${py_version} --output .))
+  package_path="${root_dir}/${py_version}/${system}/${package_name}"
 
-  package_name=$(conda-build --python $py_version --output .)
-  package_name=${package_name##*/}
-  package_path="$package_dir/$package_name"
-
-  conda-build --no-anaconda-upload --python $py_version --output-folder $py_version_dir .
+  conda-build --no-anaconda-upload \
+              --python ${py_version} \
+              --output-folder "${root_dir}/${py_version}" .
   for platform in 'osx-64' 'linux-32' 'linux-64' 'win-32' 'win-64'
   do
-    conda-convert -p $platform -o $py_version_dir $package_path
-    anaconda upload $py_version_dir/$platform/$package_name
+    conda-convert -p ${platform} -o "${root_dir}/${py_version}" ${package_path}
+    anaconda upload "${root_dir}/${py_version}/${platform}/${package_name}"
   done
 done
-
-
-
-# ================================================
-# I used a workaround above because of this bug:
-# https://github.com/conda/conda-build/issues/1957
-# intended would be:
-
-
-# root_dir=$(conda-build --output .)
-# root_dir=${root_dir%/*/*}
-#
-# for py_version in '2.7' '3.5' '3.6'
-# do
-#   py_version_dir="${root_dir}/${py_version}"
-#
-#   package_path=$(conda-build --python $py_version --output-folder $py_version_dir --output .)
-#   package_name=${package_path##*/}
-#
-#   conda-build --no-anaconda-upload --python $py_version --output-folder $py_version_dir .
-#   for platform in 'osx-64' 'linux-32' 'linux-64' 'win-32' 'win-64'
-#   do
-#     conda-convert -p $platform -o $py_version_dir $package_path
-#     anaconda upload $py_version_dir/$platform/$package_name
-#   done
-# done
