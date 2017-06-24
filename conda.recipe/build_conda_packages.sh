770a8cfcee7ef8a70b2ee667962c30eaa4d919dd
mcocdawc <oskar.weser@gmail.com>
2017-06-24 15:47:44 +0200

    MAINT: Better build_conda_package.sh

--- conda.recipe/build_conda_packages.sh
+++ conda.recipe/build_conda_packages.sh
@@ -6,22 +6,22 @@
 # https://github.com/conda/conda-build/issues/1957
 # ====================================================
 
-tmp=$(dirname $(conda-build --output .))
-system=$(basename $tmp)
-root_dir=$(dirname $tmp)
+tmp=$(dirname "$(conda-build --output .)")
+system=$(basename "$tmp")
+root_dir=$(dirname "$tmp")
 
 
-for py_version in '2.7' '3.5' '3.6'
+for py_version in 2.7 3.5 3.6
 do
-  package_name=$(basename $(conda-build --python ${py_version} --output .))
-  package_path="${root_dir}/${py_version}/${system}/${package_name}"
+  package_name=$(basename "$(conda-build --python $py_version --output .)")
+  package_path=$root_dir/$py_version/$system/$package_name
 
   conda-build --no-anaconda-upload \
               --python ${py_version} \
               --output-folder "${root_dir}/${py_version}" .
-  for platform in 'osx-64' 'linux-32' 'linux-64' 'win-32' 'win-64'
+  for platform in osx-64 linux-{32,64} win-{32,64}
   do
-    conda-convert -p ${platform} -o "${root_dir}/${py_version}" ${package_path}
-    anaconda upload --force "${root_dir}/${py_version}/${platform}/${package_name}"
+    conda-convert -p $platform -o "$root_dir/$py_version" "$package_path"
+    anaconda upload --force "$root_dir/$py_version/$platform/$package_name"
   done
 done
