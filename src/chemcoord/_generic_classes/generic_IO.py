12c0dca6175286c39c932de6a348b90c95670dad
mcocdawc <oskar.weser@gmail.com>
2017-07-10 08:39:42 +0200

    MAINT: Removed unused imports

--- src/chemcoord/_generic_classes/generic_IO.py
+++ src/chemcoord/_generic_classes/generic_IO.py
@@ -4,20 +4,9 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-from chemcoord._exceptions import PhysicalMeaning
-from chemcoord.configuration import settings
-from chemcoord.utilities import algebra_utilities
-from chemcoord.utilities.set_utilities import pick
-import chemcoord.constants as constants
-import collections
 import sympy
 from itertools import product
-import numba as nb
-from numba import jit
 import numpy as np
-import pandas as pd
-from six.moves import zip  # pylint:disable=redefined-builtin
-from sortedcontainers import SortedSet
 
 
 class GenericIO(object):
