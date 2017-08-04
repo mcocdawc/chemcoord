b17bdd4dec201ad86241218970e945bc04c0ea6f
mcocdawc <oskar.weser@gmail.com>
2017-08-04 16:38:16 +0200

    MAINT: Symmetry class renamed give to get

--- src/chemcoord/cartesian_coordinates/_cartesian_class_symmetry.py
+++ src/chemcoord/cartesian_coordinates/_cartesian_class_symmetry.py
@@ -9,9 +9,9 @@ from chemcoord.cartesian_coordinates._cartesian_class_core import CartesianCore
 
 
 class CartesianSymmetry(CartesianCore):
-    def _give_mg_molecule(self):
+    def _get_mg_molecule(self):
         return mg.Molecule(self['atom'].values,
                            self.loc[:, ['x', 'y', 'z']].values)
 
-    def give_point_group_analyzer(self):
-        return PointGroupAnalyzer(self._give_mg_molecule())
+    def get_point_group_analyzer(self):
+        return PointGroupAnalyzer(self._get_mg_molecule())
