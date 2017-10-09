004ecf3839e88ae6f419f32ba7839efec31e9fce
mcocdawc <oskar.weser@gmail.com>
2017-10-09 17:18:42 +0200

    MAIN: Improved conda.recipe

--- conda.recipe/build_conda_packages.sh
+++ conda.recipe/build_conda_packages.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-# =============================================================================
-# Please note the bug, that for conda-build the option '--output' does
-# not respect the directories given by '--output-folder':
-# https://github.com/conda/conda-build/issues/1957
-# =============================================================================
 set -e
 
-tmp=$(dirname "$(conda-build --output .)")
-system=$(basename "$tmp")
-root_dir=$(dirname "$tmp")
-
-
-for py_version in 2.7 3.5 3.6
+for py_version in 2.7 3.6
 do
   echo $py_version
-  package_name=$(basename "$(conda-build --python $py_version --output .)")
-  package_path=$root_dir/$py_version/$system/$package_name
+  package_path=$(conda-build --python $py_version \
+                             --channel matsci \
+                             --output-folder ./build/$py_version\
+                             --output \
+                             '.')
+  filename=$(basename $package_path)
 
   conda-build --no-anaconda-upload \
               --python ${py_version} \
               --channel matsci \
-              --output-folder "${root_dir}/${py_version}" \
-              "."
+              --output-folder "./build/$py_version" \
+              '.'
   for platform in osx-64 linux-{32,64} win-{32,64}
   do
     conda-convert --platform $platform \
-                  --output-dir "$root_dir/$py_version" \
-                  "$package_path"
-    anaconda upload --force "$root_dir/$py_version/$platform/$package_name"
+                  --output-dir "./build/$py_version/" \
+                  $package_path
+    anaconda upload --force "./build/$py_version/$platform/$filename"
   done
 done
