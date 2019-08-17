aea23a1bb757d36fa6c8966aef12202bba39eae4
mcocdawc <oskar.weser@gmail.com>
2019-08-17 16:26:21 +0200

    TEST: test added for #58

--- tests/internal_coordinates/Zmat/test_zmat_core.py
+++ tests/internal_coordinates/Zmat/test_zmat_core.py
@@ -4,13 +4,14 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+from os.path import join
+from io import StringIO
+from sympy import Symbol
+import pytest
+
 import chemcoord as cc
 from chemcoord.xyz_functions import allclose
-import pytest
-from chemcoord.exceptions import UndefinedCoordinateSystem, InvalidReference
-import os
-import sys
-from sympy import Symbol
 
 
 def get_script_path():
@@ -18,13 +19,13 @@ def get_script_path():
 
 
 def get_structure_path(script_path):
-    test_path = os.path.join(script_path)
+    test_path = join(script_path)
     while True:
-        structure_path = os.path.join(test_path, 'structures')
+        structure_path = join(test_path, 'structures')
         if os.path.exists(structure_path):
             return structure_path
-        else:
-            test_path = os.path.join(test_path, '..')
+
+        test_path = join(test_path, '..')
 
 
 STRUCTURE_PATH = get_structure_path(get_script_path())
@@ -34,7 +35,7 @@ def test_addition_with_sympy():
     theta, x = Symbol('theta', real=True), Symbol('x', real=True)
 
     molecule = cc.Cartesian.read_xyz(
-        os.path.join(STRUCTURE_PATH, 'MIL53_small.xyz'), start_index=1)
+        join(STRUCTURE_PATH, 'MIL53_small.xyz'), start_index=1)
 
     zmolecule = molecule.get_zmat()
 
@@ -47,3 +48,15 @@ def test_addition_with_sympy():
 
     zmolecule = zmolecule + zmolecule2
     zmolecule.subs(x, 3)
+
+
+def test_write_and_read():
+    # Created because of https://github.com/mcocdawc/chemcoord/issues/58
+    water_1 = cc.Cartesian.read_xyz(
+        join(STRUCTURE_PATH, 'water.xyz'), start_index=1)
+    z_water_str = water_1.get_zmat().to_zmat(upper_triangle=True)
+    water_2 = cc.Zmat.read_zmat(StringIO(z_water_str)).get_cartesian()
+    assert allclose(water_1, water_2, atol=1e-6)
+
+    z_water_str = water_1.get_zmat().to_zmat(upper_triangle=False)
+    cc.Zmat.read_zmat(StringIO(z_water_str)).get_cartesian()
