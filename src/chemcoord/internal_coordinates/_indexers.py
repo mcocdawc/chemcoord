1741b1e4d6ef44dc3a77fdbaae52f200a8eb8777
Oskar Weser <oskar.weser@gmail.com>
2021-12-23 15:38:28 +0100

    went back to master version indexer

--- src/chemcoord/internal_coordinates/_indexers.py
+++ src/chemcoord/internal_coordinates/_indexers.py
@@ -4,32 +4,28 @@ from chemcoord.exceptions import InvalidReference
 
 
 class _generic_Indexer(object):
-    def __getitem__(self, key):
-        if isinstance(key, tuple):
-            selected = self.indexer[key[0], key[1]]
-        else:
-            selected = self.indexer[key]
-        return selected
+    def __init__(self, molecule):
+        self.molecule = molecule
 
 
 class _Loc(_generic_Indexer):
-    def __init__(self, molecule):
-        self.molecule = molecule
-        self.indexer = self.molecule._frame.loc
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            selected = self.molecule._frame.loc[key[0], key[1]]
+        else:
+            selected = self.molecule._frame.loc[key]
+        return selected
 
-class _ILoc(_generic_Indexer):
-    def __init__(self, molecule):
-        self.molecule = molecule
-        self.indexer = self.molecule._frame.iloc
 
-class _Unsafe_base():
+class _Unsafe_Loc(_Loc):
     def __setitem__(self, key, value):
         if isinstance(key, tuple):
-            self.indexer[key[0], key[1]] = value
+            self.molecule._frame.loc[key[0], key[1]] = value
         else:
-            self.indexer[key] = value
+            self.molecule._frame.loc[key] = value
 
-class _SafeBase():
+
+class _Safe_Loc(_Loc):
     def __setitem__(self, key, value):
         if self.molecule.dummy_manipulation_allowed:
             molecule = self.molecule
@@ -37,9 +33,9 @@ class _SafeBase():
             molecule = self.molecule.copy()
 
         if isinstance(key, tuple):
-            self.indexer[key[0], key[1]] = value
+            molecule._frame.loc[key[0], key[1]] = value
         else:
-            self.indexer[key] = value
+            molecule._frame.loc[key] = value
 
         try:
             molecule.get_cartesian()
@@ -65,17 +61,46 @@ class _SafeBase():
                 pass
 
 
+class _ILoc(_generic_Indexer):
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            selected = self.molecule._frame.iloc[key[0], key[1]]
+        else:
+            selected = self.molecule._frame.iloc[key]
+        return selected
 
-class _Unsafe_Loc(_Loc, _Unsafe_base):
-    pass
+
+class _Unsafe_ILoc(_ILoc):
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            self.molecule._frame.iloc[key[0], key[1]] = value
+        else:
+            self.molecule._frame.iloc[key] = value
 
 
-class _Safe_Loc(_Loc, _SafeBase):
-    pass
+class _Safe_ILoc(_Unsafe_ILoc):
+    def __setitem__(self, key, value):
+        if self.molecule.dummy_manipulation_allowed:
+            molecule = self.molecule
+        else:
+            molecule = self.molecule.copy()
+        if isinstance(key, tuple):
+            molecule._frame.iloc[key[0], key[1]] = value
+        else:
+            molecule._frame.iloc[key] = value
 
-
-class _Unsafe_ILoc(_ILoc, _Unsafe_base):
-    pass
-
-class _Safe_ILoc(_ILoc, _SafeBase):
-    pass
+        try:
+            molecule.get_cartesian()
+        except AttributeError:
+            self.molecule = molecule
+        except InvalidReference as exception:
+            if molecule.dummy_manipulation_allowed:
+                self.molecule._insert_dummy_zmat(exception, inplace=True)
+            else:
+                exception.zmat_after_assignment = molecule
+                raise exception
+        if molecule.dummy_manipulation_allowed:
+            try:
+                self.molecule._remove_dummies(inplace=True)
+            except AttributeError:
+                pass
