a3538d252e0dea0775f9a5c2be60a4a992067668
Oskar Weser <oskar.weser@gmail.com>
2022-01-09 20:09:17 +0100

    Ship git_version in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,17 @@
 """Setup file for the chemcoord package.
 """
 
+# The git get_version samples are from Git user aebrahim
+
 from setuptools import setup, find_packages
-import version
+
+from subprocess import check_output, CalledProcessError
+from os import path, name, devnull, environ, listdir
+import re
+import shutil
+import tempfile
+
+
 
 MAIN_PACKAGE = 'chemcoord'
 DESCRIPTION = "Python module for dealing with chemical coordinates."
@@ -40,10 +49,66 @@ def readme():
         return freadme.read()
 
 
+def format_git_describe(git_str, pep440=False):
+    """format the result of calling 'git describe' as a python version"""
+    if git_str is None:
+        return None
+    if "-" not in git_str:  # currently at a tag
+        return git_str
+    else:
+        # formatted as version-N-githash
+        # want to convert to version.postN-githash
+        git_str = git_str.replace("-", ".post", 1)
+        if pep440:  # does not allow git hash afterwards
+            return git_str.split("-")[0]
+        else:
+            return git_str.replace("-g", "+git")
+
+def call_git_describe(abbrev=7):
+    """return the string output of git desribe"""
+    GIT_COMMAND = "git"
+    CURRENT_DIRECTORY = path.dirname(path.abspath(__file__))
+    try:
+        with open(devnull, "w") as fnull:
+            arguments = [GIT_COMMAND, "describe", "--tags",
+                         "--abbrev=%d" % abbrev]
+            return check_output(arguments, cwd=CURRENT_DIRECTORY,
+                                stderr=fnull).decode("ascii").strip()
+    except (OSError, CalledProcessError):
+        return None
+
+
+def get_version(pep440=False):
+    """Tracks the version number.
+
+    pep440: bool
+        When True, this function returns a version string suitable for
+        a release as defined by PEP 440. When False, the githash (if
+        available) will be appended to the version string.
+
+    The file VERSION holds the version information. If this is not a git
+    repository, then it is reasonable to assume that the version is not
+    being incremented and the version returned will be the release version as
+    read from the file.
+
+    However, if the script is located within an active git repository,
+    git-describe is used to get the version information.
+
+    The file VERSION will need to be changed by manually. This should be done
+    before running git tag (set to the same as the version in the tag).
+
+    """
+
+    git_version = format_git_describe(call_git_describe(), pep440=pep440)
+    if git_version is None:  # not a git repository
+        raise ValueError("git_version is None")
+    return git_version
+
+
 def setup_package():
     setup(
         name=MAIN_PACKAGE,
-        version=version.get_version(pep440=True),
+        version=get_version(pep440=True),
         url=URL,
         description=DESCRIPTION,
         author=AUTHOR,
