024a1f3c75d1f37519c569f80e53b022f0b682a6
Oskar Weser <oskar.weser@gmail.com>
2024-04-17 15:56:29 +0200

    check more python versions

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering :: Chemistry',
     'Topic :: Scientific/Engineering :: Physics']
 VERSION = '2.1.0'
