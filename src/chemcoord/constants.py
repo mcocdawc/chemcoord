7b92a9764ddea76ee80d7ff1da7c717960d8add5
Oskar Weser <oskar.weser@gmail.com>
2025-01-07 17:16:00 -0500

    ignore too long lines for data in CSV

--- src/chemcoord/constants.py
+++ src/chemcoord/constants.py
@@ -1,3 +1,4 @@
+# ruff: noqa: E501
 # -*- coding: utf-8 -*-
 ###############################################################################
 # If you are editing this file please consider commiting the change
