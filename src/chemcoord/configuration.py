005e2b06ffc289ca1a34c56b3d89c387695970dd
Oskar Weser <oskar.weser@gmail.com>
2025-02-21 17:11:53 -0500

    finished documentation of settings

--- src/chemcoord/configuration.py
+++ src/chemcoord/configuration.py
@@ -13,7 +13,9 @@ DEFAULT_RC_PATH: Final = Path("~/.chemcoord.yml")
 @define(hash=True, kw_only=True)
 class Defaults:
     use_lookup: bool = False
+    #: Which atomic radius data to use.
     atomic_radius_data: str = "atomic_radius_cc"
+    #: Which molecular viewer to use.
     viewer: str = "gv.exe"
     #: Which filetype to use when viewing a list of molecules.
     list_viewer_file: Literal["molden", "xyz"] = "molden"
@@ -23,19 +25,25 @@ class Defaults:
 class Settings:
     defaults: Defaults = Defaults()
 
-    def write(self, path: PathLike = DEFAULT_RC_PATH.expanduser()) -> None:
-        with open(path, "w") as f:
+    def write(self, path: PathLike = DEFAULT_RC_PATH) -> None:
+        """Write the settings to a yaml file
+
+        Args:
+            path: The path to the settings file.
+                Defaults to ``~/.chemcoord.yml``.
+        """
+        with open(Path(path).expanduser(), "w") as f:
             f.write("# Settings files for `chemcoord`.\n")
             f.write("# You can delete keys; in this case the default is taken.\n")
             yaml.dump(unstructure(self), stream=f, default_flow_style=False)
 
 
-def read(path: PathLike = DEFAULT_RC_PATH.expanduser()) -> Settings:
-    with open(path) as f:
+def _read(path: PathLike = DEFAULT_RC_PATH) -> Settings:
+    with open(Path(path).expanduser()) as f:
         return structure(yaml.safe_load(stream=f), Settings)
 
 
 if DEFAULT_RC_PATH.expanduser().exists():
-    settings = read()
+    settings = _read()
 else:
     settings = Settings()
