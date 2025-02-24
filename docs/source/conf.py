8b7b6174bf939205a39f7b973cb98f6f643b8d41
Oskar Weser <oskar.weser@gmail.com>
2025-02-24 10:33:42 -0500

    improved doc settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,21 +12,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
-import sys
 
 # on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # if on_rtd:
 #    html_theme = 'default'
 # else:
 #     sys.path.append(os.path.dirname(os.path.normpath(module_dir)))
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("../../src"))
-
 import chemcoord as cc
 
 # -- General configuration ------------------------------------------------
